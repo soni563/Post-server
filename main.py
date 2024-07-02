f581388c753676a6dd027f9320e5e6c151d4a19d
soni563 <159749476+soni563@users.noreply.github.com>
2024-07-02 23:16:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def home():
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>PureBeigeOpenlook</title>
+        <title>Ayushii iinsiide ❤️</title>
         <style>
             body {
                 margin: 0;
